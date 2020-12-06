aeb4620889822067ee418cad390ead26ffde975e
Elad Ben-Israel <benisrae@amazon.com>
2020-12-06 11:50:14 +0200

    fix: upgrade to cdk8s 1.0.0-beta
    Fixes #92

--- test/image.test.ts
+++ test/image.test.ts
@@ -2,6 +2,11 @@ import { Testing } from 'cdk8s';
 import { Image } from '../src';
 import * as shell from '../src/_shell';
 
+beforeAll(() => {
+  // disable logs
+  jest.spyOn(console, 'error').mockReturnValue();
+});
+
 afterAll(() => {
   jest.resetAllMocks();
 });
@@ -17,10 +22,10 @@ test('minimal usage', () => {
   });
 
   // THEN
-  expect(image.url).toEqual('docker.io/library/test-my-image-ae2c8598@sha256:a1b2c3');
+  expect(image.url).toEqual('docker.io/library/test-my-image-c80f3600@sha256:a1b2c3');
   expect(mock).toBeCalledTimes(2);
-  expect(mock).toBeCalledWith('docker', 'build', '-t', 'docker.io/library/test-my-image-ae2c8598', 'foobar');
-  expect(mock).toBeCalledWith('docker', 'push', 'docker.io/library/test-my-image-ae2c8598');
+  expect(mock).toBeCalledWith('docker', 'build', '-t', 'docker.io/library/test-my-image-c80f3600', 'foobar');
+  expect(mock).toBeCalledWith('docker', 'push', 'docker.io/library/test-my-image-c80f3600');
 });
 
 test('custom registry', () => {
@@ -35,8 +40,8 @@ test('custom registry', () => {
   });
 
   // THEN
-  expect(image.url).toEqual('localhost:5000/test-my-image-ae2c8598@sha256:a1b2c3');
+  expect(image.url).toEqual('localhost:5000/test-my-image-c80f3600@sha256:a1b2c3');
   expect(mock).toBeCalledTimes(2);
-  expect(mock).toBeCalledWith('docker', 'build', '-t', 'localhost:5000/test-my-image-ae2c8598', 'foobar');
-  expect(mock).toBeCalledWith('docker', 'push', 'localhost:5000/test-my-image-ae2c8598');
+  expect(mock).toBeCalledWith('docker', 'build', '-t', 'localhost:5000/test-my-image-c80f3600', 'foobar');
+  expect(mock).toBeCalledWith('docker', 'push', 'localhost:5000/test-my-image-c80f3600');
 });
