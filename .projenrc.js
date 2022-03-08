107250ac325a95fdb857f72ea4be18253e2a9506
Tal Vintrob <vintrob@cisco.com>
2022-03-08 13:57:17 +0200

    feat(build): added python build and publish to jfrog

--- .projenrc.js
+++ .projenrc.js
@@ -13,7 +13,7 @@ const project = new cdk.JsiiProject({
     'constructs',
     'cdk8s',
   ],
-  name: 'cdk8s-image',
+  name: 'udp-cdk8s-image',
   minNodeVersion: '14.17.0',
   description: 'Build & Push local docker images inside CDK8s applications',
   repository: 'https://github.com/cdk8s-team/cdk8s-image',
@@ -24,6 +24,13 @@ const project = new cdk.JsiiProject({
     secret: 'GITHUB_TOKEN',
   },
   autoApproveUpgrades: true,
+  publishToPypi: {
+    distName: 'udp-cdk8s-image',
+    module: 'udp_cdk8s_image',
+    twineRegistryUrl: 'https://fso.jfrog.io/artifactory/api/pypi/fso-pypi',
+    twineUsernameSecret: 'ARTIFACTORY_USERNAME',
+    twinePasswordSecret: 'ARTIFACTORY_TOKEN',
+  },
 });
 
 project.synth();
