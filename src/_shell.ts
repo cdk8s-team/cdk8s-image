841946aaefdf881a5a8ae31727d7a4d4c0cfe2a5
cdk8s-automation <81352262+cdk8s-automation@users.noreply.github.com>
2023-07-06 11:52:47 -0400

    chore(deps): upgrade configuration (#908)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdk8s-team/cdk8s-image/actions/runs/5470161630
    
    ------
    
    *Automatically created by projen via the "upgrade-configuration-main" workflow*

--- src/_shell.ts
+++ src/_shell.ts
@@ -1,6 +1,6 @@
 import { spawnSync } from 'child_process';
 
-export function shell(command: string, ...args: string[]) {
+export function shell(command: string, ...args: string[]): string {
   const proc = spawnSync(command, args);
 
   if (proc.error) {
@@ -11,5 +11,5 @@ export function shell(command: string, ...args: string[]) {
     throw new Error(`non-zero exist code ${proc.status}: ${proc.stdout} ${proc.stderr}`);
   }
 
-  return proc.stdout;
+  return proc.stdout.toString('utf-8');
 }
